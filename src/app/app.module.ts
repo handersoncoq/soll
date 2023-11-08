53cb4ceca14bf5ffe5a38634919508abe29d052c
Handerson COQ <handersoncoq@Handersons-MacBook-Air.local>
2023-11-08 03:06:14 -0500

    created basic front page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FrontPageComponent } from './components/front-page/front-page.component';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +20,13 @@ import { FrontPageComponent } from './components/front-page/front-page.component
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/front-page/front-page.component.ts
+++ src/app/components/front-page/front-page.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-front-page',
   templateUrl: './front-page.component.html',
   styleUrls: ['./front-page.component.scss']
 })
-export class FrontPageComponent {
+export class FrontPageComponent implements OnInit{
+
+  slogan = "A better way to save money between friends and loved ones. Learn about Soll's different systems.";
+  learnMore = 'We have a system for everyone, based on your need.'
+  fontPageImage = './assets/img/font-page-img.png'
+
+  ngOnInit() {
+    this.loadMaterialSymbols();
+  }
+
+  loadMaterialSymbols() {
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200';
+    document.head.appendChild(link);
+  }
 
 }
