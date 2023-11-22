3c6455264125ec3d29c71a65c31dee23ceaec0d2
Handerson Coq <coqhandersonth@gmail.com>
2023-11-22 00:00:28 -0500

    re-designed hero secrion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { ContentManagerService } from './services/content-manager/content-manager.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Soll';
+  showLogo = false;
+  appLogo!: string;
+
+  constructor(private contentManagerService: ContentManagerService){
+    this.appLogo = this.contentManagerService.getAppLogo3();
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    if (window.scrollY > 20) {
+      this.showLogo = true;
+    } else {
+      this.showLogo = false;
+    }
+  }
+
+  scrollToTop(){
+    window.scroll({
+      top: 0,
+      behavior: 'smooth'})
+  }
 }
