f0204c5d611eb526a28eb27bc22da2be41e2b61b
Handerson Coq <coqhandersonth@gmail.com>
2025-11-06 06:36:39 -0500

    attempt 3: fixing email verification link setup

--- src/app/firebase.config.ts
+++ src/app/firebase.config.ts
@@ -1,7 +1,9 @@
+const env = (import.meta as any).env || (window as any);
+
 export const firebaseConfig = {
-  apiKey: (window as any)['NG_APP_FIREBASE_API_KEY'],
-  authDomain: (window as any)['NG_APP_FIREBASE_AUTH_DOMAIN'],
-  projectId: (window as any)['NG_APP_FIREBASE_PROJECT_ID'],
-  appId: (window as any)['NG_APP_FIREBASE_APP_ID'],
-  measurementId: (window as any)['NG_APP_FIREBASE_MEASUREMENT_ID'],
+  apiKey: env['NG_APP_FIREBASE_API_KEY'] || '',
+  authDomain: env['NG_APP_FIREBASE_AUTH_DOMAIN'] || '',
+  projectId: env['NG_APP_FIREBASE_PROJECT_ID'] || '',
+  appId: env['NG_APP_FIREBASE_APP_ID'] || '',
+  measurementId: env['NG_APP_FIREBASE_MEASUREMENT_ID'] || '',
 };
