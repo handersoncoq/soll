1660e50a0b0c0ee8c92a37aa38594b8cf56a67b3
Handerson Coq <coqhandersonth@gmail.com>
2025-11-08 15:21:39 -0500

    using process.env

--- src/app/firebase.config.ts
+++ src/app/firebase.config.ts
@@ -1,9 +1,7 @@
-const env = (import.meta as any).env || (window as any);
-
 export const firebaseConfig = {
-  apiKey: env['NG_APP_FIREBASE_API_KEY'] || '',
-  authDomain: env['NG_APP_FIREBASE_AUTH_DOMAIN'] || '',
-  projectId: env['NG_APP_FIREBASE_PROJECT_ID'] || '',
-  appId: env['NG_APP_FIREBASE_APP_ID'] || '',
-  measurementId: env['NG_APP_FIREBASE_MEASUREMENT_ID'] || '',
+  apiKey: process.env['NG_APP_FIREBASE_API_KEY'],
+  authDomain: process.env['NG_APP_FIREBASE_AUTH_DOMAIN'],
+  projectId: process.env['NG_APP_FIREBASE_PROJECT_ID'],
+  appId: process.env['NG_APP_FIREBASE_APP_ID'],
+  measurementId: process.env['NG_APP_FIREBASE_MEASUREMENT_ID'],
 };
