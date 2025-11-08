c8dbb14aa7f21ef017aa63c06e9c0c99db6ed74c
Handerson Coq <coqhandersonth@gmail.com>
2025-11-08 16:12:35 -0500

    using file transfer for env variables

--- src/app/config/environment.ts
+++ src/app/config/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: process.env['NG_APP_FIREBASE_API_KEY'] ?? '',
-    authDomain: process.env['NG_APP_FIREBASE_AUTH_DOMAIN'] ?? '',
-    projectId: process.env['NG_APP_FIREBASE_PROJECT_ID'] ?? '',
-    appId: process.env['NG_APP_FIREBASE_APP_ID'] ?? '',
-    measurementId: process.env['NG_APP_FIREBASE_MEASUREMENT_ID'] ?? '',
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    appId: '',
+    measurementId: '',
   },
 };
---
+++ src/set-env.js
@@ -0,0 +1,22 @@
+import fs from "fs";
+
+const targetPath = "./src/app/config/environment.ts";
+
+const envConfigFile = `
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: '${process.env.NG_APP_FIREBASE_API_KEY}',
+    authDomain: '${process.env.NG_APP_FIREBASE_AUTH_DOMAIN}',
+    projectId: '${process.env.NG_APP_FIREBASE_PROJECT_ID}',
+    appId: '${process.env.NG_APP_FIREBASE_APP_ID}',
+    measurementId: '${process.env.NG_APP_FIREBASE_MEASUREMENT_ID}',
+  },
+};
+`;
+
+try {
+  fs.writeFileSync(targetPath, envConfigFile);
+} catch (error) {
+  console.error("Error writing environment file:", error);
+}
